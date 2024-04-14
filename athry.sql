6df4d798e81f2b43fb84e46b7c4c98d4c79f8d10
mohamed gamal <mohamedgammal55@gmail.com>
2024-04-14 02:47:22 +0200

    first commit

---
+++ athry.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2024 at 02:45 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `athry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `book_id` int(20) NOT NULL,
+  `book_date` date NOT NULL,
+  `person_no` int(20) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `tour_id` int(20) NOT NULL,
+  `Guide_id` int(20) NOT NULL,
+  `Client_id` int(20) NOT NULL,
+  `payment_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `city_id` int(20) NOT NULL,
+  `city_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `client_id` int(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `active` bit(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `name`, `mail`, `password`, `phone`, `image`, `active`) VALUES
+(5, '23qwesd', 'mohamedgammal55@gmail.com', '$2y$10$Cv/pPdo8aYAO/TA8nB/QMe/D1H.uHLnV2aThnx4OdDLogxlJR.MHq', '123456', '', b'00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guides`
+--
+
+CREATE TABLE `guides` (
+  `guide_id` int(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `mail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `active` bit(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guides`
+--
+
+INSERT INTO `guides` (`guide_id`, `name`, `mail`, `password`, `phone`, `image`, `active`) VALUES
+(2, 'admin@admin.com', 'mohamedgammal55@gmail.com', '$2y$10$T/9IfiFyyqrJP1UVF4QSwO7ONxKEbiKaJbxuhz62zqjRmOCvKUHq6', '212345', '', b'00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `landmarks`
+--
+
+CREATE TABLE `landmarks` (
+  `landmark_id` int(20) NOT NULL,
+  `landmark_name` varchar(255) NOT NULL,
+  `landmark_site` varchar(255) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `description` text NOT NULL,
+  `city_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(20) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `client_id` int(20) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `pay_id` int(20) NOT NULL,
+  `pay_name` varchar(255) NOT NULL,
+  `pay_no` varchar(255) NOT NULL,
+  `valid_date` date NOT NULL,
+  `cvv` varchar(255) NOT NULL,
+  `client_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tours`
+--
+
+CREATE TABLE `tours` (
+  `tour_id` int(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(20) NOT NULL,
+  `person_no` int(20) NOT NULL,
+  `Date_from` date NOT NULL,
+  `Date_to` date NOT NULL,
+  `landmark_id` int(20) NOT NULL,
+  `Guide_id` int(20) NOT NULL,
+  `City_id` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `book_guide_fk` (`Guide_id`),
+  ADD KEY `book_tour_fk` (`tour_id`),
+  ADD KEY `book_pay_fk` (`payment_id`),
+  ADD KEY `book_client_id` (`Client_id`);
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `guides`
+--
+ALTER TABLE `guides`
+  ADD PRIMARY KEY (`guide_id`);
+
+--
+-- Indexes for table `landmarks`
+--
+ALTER TABLE `landmarks`
+  ADD PRIMARY KEY (`landmark_id`),
+  ADD KEY `landmark_cities` (`city_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `message_client_fk` (`client_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`pay_id`),
+  ADD KEY `pay_client_fk` (`client_id`);
+
+--
+-- Indexes for table `tours`
+--
+ALTER TABLE `tours`
+  ADD PRIMARY KEY (`tour_id`),
+  ADD KEY `tour_guide` (`Guide_id`),
+  ADD KEY `tour_cities` (`City_id`),
+  ADD KEY `tour_land` (`landmark_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `book_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `city_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `client_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `guides`
+--
+ALTER TABLE `guides`
+  MODIFY `guide_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `landmarks`
+--
+ALTER TABLE `landmarks`
+  MODIFY `landmark_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `pay_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tours`
+--
+ALTER TABLE `tours`
+  MODIFY `tour_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `book_client_id` FOREIGN KEY (`Client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_guide_fk` FOREIGN KEY (`Guide_id`) REFERENCES `guides` (`guide_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_pay_fk` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`pay_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_tour_fk` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`tour_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `landmarks`
+--
+ALTER TABLE `landmarks`
+  ADD CONSTRAINT `landmark_cities` FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `message_client_fk` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `pay_client_fk` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tours`
+--
+ALTER TABLE `tours`
+  ADD CONSTRAINT `tour_cities` FOREIGN KEY (`City_id`) REFERENCES `cities` (`city_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tour_guide` FOREIGN KEY (`Guide_id`) REFERENCES `guides` (`guide_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tour_land` FOREIGN KEY (`landmark_id`) REFERENCES `landmarks` (`landmark_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
